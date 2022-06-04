19e504c2d8dab6bb7b0cf2276f5f01e06e02cebe
Syizeruby <106030812+Syizeruby@users.noreply.github.com>
2022-06-03 22:17:30 -0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,2 +1,1 @@
-
-wget https://www.mediafire.com/file/ihpgjf7q4em5dnh/apps-debian-rootfs.tar.gz
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=18QawJUlFKIYJnj6O0gTAh_BTnKfyyA3K' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=18QawJUlFKIYJnj6O0gTAh_BTnKfyyA3K" -O apps-debian-rootfs.tar.gz && rm -rf /tmp/cookies.txt
